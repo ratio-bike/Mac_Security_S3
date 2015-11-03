894b66fdde0370159c8d4ac1c085a4cf047917cf
godthem90 <godthem90@gmail.com>
2015-11-03 20:46:40 +0900

    Sort out

--- README_JJHui-Macbook-Air.local_Aug-17-152756-2015_Conflict.md
+++
@@ -1,4 +0,0 @@
-# Mac_Security_S3
-
-Refer the link below
-https://trmm.net/Thunderstrike_31c3
--- Venamis/test/physical_addr_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int *test = 0x1000f2804;
-	int res = *test;
-
-	printf("%0x\n",res);
-}
